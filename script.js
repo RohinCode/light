e474586814d95c98688a760106960b1502279813
RohinCode <Amir.reza.aab.2244@gmail.com>
2026-07-15 13:44:44 +0330

    22

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const send = document.getElementById("send");
 const inputSms = document.getElementById("inputSms");
 const messagePage = document.querySelector("main");
 const openMenuBtn = document.getElementById("openMenuBtn");
+const otherMsg = document.querySelectorAll(".other");
 const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("sidebar-overlay");
-
-
+const others = document.querySelectorAll(".otherPersons");
+const name = document.querySelector("#name");
+const profileImg = document.querySelector(".profile img");
 //-------------------نمایش پیام نوشته شده توسط من----------
 send.addEventListener("click", () => {
     sendMessage(inputSms.value);
 });
 
 inputSms.addEventListener("keydown", (e) => {
+    if (window.innerWidth <= 900) return;
     if (e.key === "Enter" && !e.shiftKey) {
         e.preventDefault();
         sendMessage(inputSms.value);
@@ -23,21 +26,26 @@ inputSms.addEventListener("keydown", (e) => {
 inputSms.addEventListener("input", () => {
     inputSms.style.height = "55px";
     inputSms.style.height = inputSms.scrollHeight + "px";
+    inputSms.dir = /[\u0590-\u08FF]/.test(inputSms.value) ? "rtl" : "ltr";
 });
 
 function sendMessage(text) {
     if (!text.trim()) return;
     const message = document.createElement("div");
     message.classList.add("message", "me");
+    text = text.trim(); // میخوام فاصله های اضافی پاک شه
     message.textContent = text;
+    message.dir = /[\u0590-\u08FF]/.test(text) ? "rtl" : "ltr";
+
     messagePage.appendChild(message);
-    inputSms.value = "";
     message.scrollIntoView({
         behavior: "smooth",
         block: "end",
     });
     inputSms.style.height = "55px";
     inputSms.focus();
+    inputSms.value = "";
+    inputSms.dir = "rtl"; // یا "ltr"، هر کدوم رو به عنوان حالت پیش‌فرض می‌خوای
 }
 // ---------------------نمایش منو---------------------
 
@@ -61,3 +69,24 @@ function closeSidebar() {
     sidebar.classList.remove("open");
     overlay.classList.remove("show");
 }
+
+// ------------------------------صفحه‌ی چت----------------------------------
+
+sidebar.addEventListener("click", (e) => {
+    const person = e.target.closest(".otherPersons");
+    if (person) {
+        name.innerText = person.textContent.trim();
+        if (person.textContent.trim() === "پیام‌های ذخیره") {
+            profileImg.src = "public/images/savedMessage.png";
+
+            otherMsg.forEach((i) => {
+                i.style.display = "none";
+            });
+        } else {
+            profileImg.src = "public/images/images.jpg";
+            otherMsg.forEach((i) => {
+                i.style.display = "block";
+            });
+        }
+    }
+});
